fc9a6f7135e7265c1bf3a3997af32e92f4ec3e2b
Yamil Hernandez <yjhb1999@gmail.com>
2022-07-28 17:32:01 -0400

    first commit

---
+++ javascript/allergies/allergies.js
@@ -0,0 +1,33 @@
+//
+// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Allergies {
+  constructor(score = 0) {
+    this.score = score;
+    this.allergies = allergies;
+  }
+
+  list() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  allergicTo() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+
+}
+
+const allergies = {
+  "eggs": 1,
+  "peanuts": 2,
+  "shellfish": 4,
+  "strawberries": 8,
+  "tomatoes" : 16,
+  "chocolate" : 32,
+  "pollen": 64,
+  "cats": 128
+
+};
